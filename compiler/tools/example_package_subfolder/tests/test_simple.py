2284bb2ff981e29639f5a910f2082bbe2a193357
SaeHie Park <saehie.park@samsung.com>
2024-11-21 07:16:44 +0900

    init src and tests

---
+++ compiler/tools/example_package_subfolder/tests/test_simple.py
@@ -0,0 +1,14 @@
+# from https://github.com/pypa/sampleproject/
+
+import unittest
+
+import example_package_sean.example as example
+
+
+class TestExample(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(example.hello('world'), 'hello world')
+
+
+if __name__ == '__main__':
+    unittest.main()
